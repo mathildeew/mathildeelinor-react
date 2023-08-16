740148b3eb3d334e06217066b4ba4d2926fe7ca2
mathildeew <mathildewiik@hotmail.com>
2023-08-16 10:39:07 +0200

    adds main and navbar to layout

--- src/components/ui/Layout/index.jsx
+++ src/components/ui/Layout/index.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 export default function Layout() {
   return (
     <>
-      <Outlet />
-      <Footer />
+      <main>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </main>
     </>
   );
 }
