f68706546a958b77af5dd01c11e5b409b43f72a2
mathildeew <mathildewiik@hotmail.com>
2023-08-20 14:04:35 +0200

    moved about to home page

--- src/components/Home/HomeContainer.styles.jsx
+++ src/components/Home/HomeContainer.styles.jsx
@@ -2,24 +2,126 @@ import styled from "styled-components";
 import { displayFlex } from "../../../styles/mixins";
 
 export const HomeContainer = styled.section`
-  height: 80vh;
-
   .intro {
     ${displayFlex({
       direction: "column",
-      justify: "center",
+      justify: "",
     })}
+    overflow: hidden;
   }
 
   h1 {
-    font-size: 15vw;
+    font-size: 13vw;
     font-weight: 500;
-    margin-bottom: 40px;
+    margin-bottom: 60px;
+  }
+
+  h3 {
+    margin-bottom: 5px;
+  }
+
+  .blobContainer {
+    ${displayFlex({
+      direction: "column",
+      justify: "space-between",
+    })}
+    height: 550px;
+    margin-bottom: 50px;
+
+    .blobText {
+      font-family: Fromage, serif;
+      font-size: 2.5rem;
+      overflow: hidden;
+    }
+  }
+
+  .blob {
+    width: 270px;
+    height: 350px;
+    background-color: var(--color-primary);
+    border-radius: 40% 56% 72% 28% / 42% 42% 56% 48%;
+    margin: 0 auto;
+    margin-bottom: 50px;
+    animation: morph 2.75s linear infinite;
+
+    ${displayFlex({
+      direction: "column",
+      justify: "center",
+      align: "center",
+    })}
+  }
+
+  @keyframes morph {
+    0%,
+    100% {
+      border-radius: 42% 56% 72% 28% / 42% 42% 56% 48%;
+    }
+    33% {
+      border-radius: 72% 28% 48% 48% / 28% 28% 72% 72%;
+    }
+    66% {
+      border-radius: 100% 56% 56% 100% / 100% 100% 56% 56%;
+    }
+  }
+
+  .portrait {
+    width: 200px;
+    height: 260px;
+    background: url("../../../public/self2.jpg");
+    background-size: cover;
+    background-position: center center;
+    border-radius: 40% 56% 72% 28% / 42% 42% 56% 48%;
+    /* border-top-right-radius: 50%;
+    border-top-left-radius: 50%; */
+    /* box-shadow: 6px -6px 0px 0px var(--color-secondary); */
+  }
+
+  #text p:first-child {
+    margin-bottom: 20px;
+  }
+
+  .bulk {
+    margin-bottom: 30px;
+
+    div:first-child {
+      display: flex;
+    }
+
+    p:first-child {
+      margin-right: 7px;
+    }
+  }
+
+  .boldText {
+    font-weight: 600;
+  }
+
+  .part {
+    overflow: hidden;
   }
 
   @media (min-width: 768px) {
     h1 {
-      font-size: 11.5rem;
+      font-size: 8rem;
+    }
+
+    .blobContainer {
+      ${displayFlex({
+        direction: "row",
+        align: "center",
+        justify: "space-between",
+      })}
+
+      .blobText {
+        width: 50%;
+      }
+      p {
+        font-size: 1.8rem;
+      }
+    }
+
+    .blob {
+      margin-left: 0;
     }
   }
 `;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,4 +1,6 @@
 import { motion as m } from "framer-motion";
+import { Link } from "react-router-dom";
+import { BaseButton } from "../../../styles/Button";
 import { HomeContainer } from "./HomeContainer.styles";
 
 export default function Home() {
@@ -13,18 +15,96 @@ export default function Home() {
           Mathilde Elinor
         </m.h1>
 
-        <m.div
-          animate={{ y: 0 }}
-          initial={{ y: "100%" }}
-          transition={{ delay: 0.5, duration: 0.5 }}
-        >
-          <p>Front-end developer.</p>
-          <p>
-            I like to craft solid, accessible and engaging products, bringing
-            ideas to life.
-          </p>
-        </m.div>
+        <div className="blobContainer">
+          <m.div className="blobText">
+            <m.h2
+              animate={{ opacity: 1 }}
+              initial={{ opacity: 0 }}
+              transition={{ delay: 0.5, duration: 0.8 }}
+            >
+              Front-end developer
+            </m.h2>
+            <m.p
+              animate={{ y: 0 }}
+              initial={{ y: "100%" }}
+              transition={{ delay: 0.5, duration: 0.5 }}
+            >
+              I like to craft solid, accessible and engaging products, bringing
+              ideas to life.
+            </m.p>
+          </m.div>
+          <m.div
+            className="blob"
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            transition={{ delay: 0.8, duration: 0.8 }}
+          >
+            <div className="portrait"></div>
+          </m.div>
+        </div>
       </div>
+
+      <m.div
+        id="about"
+        animate={{ y: 0, opacity: 1 }}
+        initial={{ y: "10%", opacity: 0 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+      >
+        <h2>About</h2>
+
+        <div id="text" className="part">
+          <p>
+            As long as I can remember I have been drawn to combining technology
+            and design. The infinite potential has always fascinated me and I
+            have never been afraid to just jump in and give it a go, whether it
+            is Microsoft Paint, HTML/CSS, blogging, World of Worldcraft or
+            Photoshop. Unfortunately for me, I always considered these as mere
+            hobbies.
+          </p>
+          <p>
+            Fast forward to 2021 and I decided to step away from my optometrist
+            career and pursue my long-lasting yearning of a technology and
+            design based occupation. As I am finishing up the front-end
+            development education at Noroff, I am thrilled to finally be able to
+            create, design and maintain user-friendly concepts that solves real
+            problems and has a purpose.
+          </p>
+        </div>
+
+        <BaseButton className="part">
+          <Link to="../../../public/cv0.pdf" target="_blank">
+            My CV
+          </Link>
+        </BaseButton>
+
+        <section className="part">
+          <div className="bulk">
+            <h2>Skills</h2>
+            <p>
+              Web accessibility, SE Optimization, FE Optimization, responsive
+              web design, testing.
+            </p>
+          </div>
+
+          <div className="bulk">
+            <h3>Languages</h3>
+            <p>JavaScript, CSS, Sass, HTML, Git</p>
+          </div>
+
+          <div className="bulk">
+            <h3>Frameworks & libraries</h3>
+            <p>React, Bootstrap, Jest</p>
+          </div>
+
+          <div className="bulk">
+            <h3>Tools & platforms</h3>
+            <p>
+              GitHub, WordPress, Cypress, Netlify, Adobe XD, Figma, Photoshop,
+              Illustrator
+            </p>
+          </div>
+        </section>
+      </m.div>
     </HomeContainer>
   );
 }
