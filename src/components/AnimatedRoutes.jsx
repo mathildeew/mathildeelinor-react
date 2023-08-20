48d932a31825cee4842c352272b490eca812e395
mathildeew <mathildewiik@hotmail.com>
2023-08-20 15:02:17 +0200

    cleaned up

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,9 +1,7 @@
 import { AnimatePresence } from "framer-motion";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./Home";
-import About from "./About";
 import Portfolio from "./Portfolio";
-import Contact from "./Contact";
 import Layout from "./ui/Layout";
 import NotFound from "./NotFound";
 
@@ -15,9 +13,7 @@ export default function AnimatedRoutes() {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
           <Route path="portfolio" element={<Portfolio />} />
-          <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
