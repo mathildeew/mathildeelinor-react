432d83a387d39debd1e5719b81ca4569ad02cdd8
mathildeew <mathildewiik@hotmail.com>
2023-10-04 17:26:44 +0200

    moves components to right folder, and deletes unused components

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,10 +1,10 @@
 import { AnimatePresence } from "framer-motion";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { useEffect } from "react";
-import Home from "./Home";
-import Portfolio from "./Portfolio";
+import Home from "../pages/Home";
+import Portfolio from "../pages/Portfolio";
 import Layout from "./ui/Layout";
-import NotFound from "./NotFound";
+import NotFound from "../pages/NotFound";
 
 export default function AnimatedRoutes() {
   const location = useLocation();
