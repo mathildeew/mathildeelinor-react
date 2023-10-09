6cb966ae9352bff76d4bdaa9c20c2330bed5ad3c
mathildeew <mathildewiik@hotmail.com>
2023-10-09 13:48:56 +0200

    adds new project and more details

--- src/js/projects.js
+++ src/js/projects.js
@@ -4,18 +4,21 @@ export const projects = [
     name: "Holidaze",
     title: "Project Exam 2",
     description:
-      "The final exam at Noroff. Delivery is late in October, so please have a look at the repository & my Notion Workspace to follow along.",
-    image: "../../public/projects/comingsoon.png",
+      "The final exam project at Noroff. The project is an extensive application that showcase all skills gained throughout the last two years. Holidaze is an accommodation booking site that provides the customer-facing side where users can book holidays at a venue, and an admin-facing side where users can register and manage venues and bookings at those venues.",
+    languages:
+      "Built with React+Vite, styled-components, JavaScript, HTML and Noroff Holidaze API.",
+    image: "../../public/projects/holidaze-landingpage.jpg",
     github: "https://github.com/mathildeew/project-exam2-holidaze",
-    link: "",
+    link: "https://holidaze-projectexam2.netlify.app/",
   },
   {
     id: 5,
     name: "Sjåpp",
     title: "JavaScript Frameworks CA",
     description:
-      "Made in the final semester. The goal was to apply knowledge of React to build an eCom store. The project covers the front-end application for an already existing API.",
-    languages: "Built with React & Styled Components.",
+      "Made in the final semester. The goal was to apply knowledge of React to build an eCom store, simulating a real-world online shopping experience. It features product display, search function, detailed product pages, cart management with context and reducer and a checkout process.",
+    languages:
+      "Built with React+Vite, styled-components, JavaScript, HTML and Noroff Online Shop API.",
     image: "../../public/projects/sjaapp.png",
     github: "https://github.com/mathildeew/js-frameworks-ca",
     link: "https://sjaapp.netlify.app/",
@@ -25,8 +28,8 @@ export const projects = [
     name: "Gavel",
     title: "Semester Project 2",
     description:
-      "Gavel is an online auction site. The API functionality is managed by an existing application, and the project only covers the front-end application for the API.",
-    languages: "Built with JavaScript, HTML & SCSS.",
+      "Gavel is an online auction site. The API functionality is managed by an existing application, and the project covers the front-end application for the API. It features user authentication, listing auctions and bidding. The project incorporates Prettier for code formatting, ESLint for linting and Cypress for E2E testing.",
+    languages: "Built with JavaScript, HTML & Bootstrap.",
     image: "../../public/projects/gavel.png",
     github: "https://github.com/mathildeew/semester-project-2",
     link: "https://gavel-sp2.netlify.app/",
