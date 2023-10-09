18932edd973c029990829d912163dca55b9a939c
mathildeew <mathildewiik@hotmail.com>
2023-10-09 15:19:53 +0200

    removes guide border

--- src/components/ui/Layout/Footer/index.jsx
+++ src/components/ui/Layout/Footer/index.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full h-11 flex justify-center items-center  border-2 border-black">
+    <footer className="w-full h-11 flex justify-center items-center mt-20">
       <p>© 2023 Mathilde Elinor Wiik</p>
     </footer>
   );
