b2842a38c1871b00c2fa9304517dafc51822f973
Mathilde Wiik <mathilde@Mathildes-MBP.home>
2024-01-07 18:51:31 +0100

    updates link path

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     id: 1,
     name: "Lillestrøm Optikk",
     role: "Visuell identitet, design og utvikling",
-    image: "/public/projects/lillestromOptikk/lillestromoptikk-banner.png",
+    image: "/dist/projects/lillestromOptikk/lillestromoptikk-banner.png",
     github: "https://github.com/mathildeew/lillestromoptikk",
     link: "https://lillestromoptikk.no",
     internal: `prosjekter/lillestrom-optikk`,
